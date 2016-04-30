aad10c0fa8e5704059bf1011b2725690f87baa26
saarsinai <saars7@gmail.com>
2016-04-30 15:54:23 +0300

    feat(user-profile): start the view of the user details

---
+++ www/app/user-profile/user-profile.controller.js
@@ -0,0 +1,40 @@
+angular.module('starter')
+  .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+    $stateProvider.state('app.userProfile', {
+      url: '/userProfile',
+      views: {
+        'menuContent': {
+          templateUrl: 'app/user-profile/user-profile.html',
+          controller: 'UserProfileCtrl'
+        }
+      }
+    })
+  })
+  .controller('UserProfileCtrl', function ($scope, $stateParams, Resource, image, $timeout, ionicMaterialInk, ionicMaterialMotion) {
+
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.isExpanded = true;
+    $scope.$parent.setExpanded(true);
+    $scope.$parent.setHeaderFab(false);
+
+    // Activate ink for controller
+    ionicMaterialInk.displayEffect();
+
+    ionicMaterialMotion.pushDown({
+    selector: '.push-down'
+    });
+    ionicMaterialMotion.fadeSlideInRight({
+    selector: '.animate-fade-slide-in .item'
+    });
+
+    const User = Resource.new("user");
+
+    // TEMP USE: untill we can get session user (login screen)
+    User.query().$promise.then(function(users, err){
+      if (users != null){
+        $scope.profile = users[0];
+      }
+    });
+
+  });
