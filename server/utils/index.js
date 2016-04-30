9bd9dd8f2fadf164d97ffb79f95b7bdfa758dedb
Tamir Lavi <tamir168@gmail.com>
2016-04-30 18:00:50 +0300

    fix(bye-crypt): removed bcrypt and replaced it with working garbage hash :)

--- server/utils/index.js
+++ server/utils/index.js
@@ -1,14 +1,9 @@
-import bcrypt from 'bcrypt';
+import hashFunc from 'object-hash';
 
 export function hash(property) {
   return function(next) {
     let user = this;
-    bcrypt.hash(user[property], /* saltRounds */ 10, function(err, hash) {
-      if (err) return next(err);
-
-      // override the cleartext password with the hashed one
-      user[property] = hash;
-      next();
-    });
+    user[property] = hashFunc(user[property]);
+    next();
   };
-}
+};
