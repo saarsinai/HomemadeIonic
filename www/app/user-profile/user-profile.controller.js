9d4be5451ccad05c3448b422cddf995e9501eea4
saarsinai <saars7@gmail.com>
2016-05-01 19:15:05 +0300

    no starter - homemade!

--- www/app/user-profile/user-profile.controller.js
+++ www/app/user-profile/user-profile.controller.js
@@ -1,4 +1,4 @@
-angular.module('starter')
+angular.module('homemade')
   .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
     $stateProvider.state('app.userProfile', {
       url: '/userProfile',
