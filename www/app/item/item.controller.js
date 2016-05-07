7ab83f2687dc5162e5a00cb76019af785898ed1d
saarsinai <saars7@gmail.com>
2016-05-08 00:44:46 +0300

    feat(item-view): order function now works, add order to the purchase history!

--- www/app/item/item.controller.js
+++ www/app/item/item.controller.js
@@ -10,58 +10,112 @@ angular.module('homemade')
       }
     })
   })
-  .controller('itemController', ['$scope', '$stateParams', 'Resource', 'Authorization', function ($scope, $stateParams, Resource, Authorization) {
+  .controller('itemController', ['$scope', '$stateParams', 'Resource', 'Authorization', '$ionicPopup', '$state',
+    function ($scope, $stateParams, Resource, Authorization, $ionicPopup, $state) {
 
-    const Item = Resource.new("item", {
-      'ofSeller': {
-        method: 'GET',
-        params: {populate: 'seller img'}
-      }
-    });
-    const userId = Authorization.getUser()._id;
+      $scope.showPopup = function () {
+        $scope.purchase = {}
 
-    var getItemById = function () {
-      Item.ofSeller({id: $stateParams.itemId})
-        .$promise.then(function (item) {
+        // An elaborate, custom popup
+        var myPopup = $ionicPopup.show({
+          template: '<input type="text" ng-model="purchase.amount">',
+          title: 'Enter order amount',
+          scope: $scope,
+          buttons: [
+            {text: 'Cancel'},
+            {
+              text: '<b>Finish</b>',
+              type: 'button-positive',
+              onTap: function (e) {
+                if (!$scope.purchase.amount) {
+                  //don't allow the user to close unless he enters wifi password
+                  e.preventDefault();
+                } else {
+                  return $scope.purchase.amount;
+                }
+              }
+            },
+          ]
+        });
+        myPopup.then(function (res) {
+          if (res && res > 0) {
 
-          $scope.item = item;
-          if ($scope.item.likes.filter(function (currUser) {
-              return currUser === userId;
-            }).length) {
-            $scope.LikeTitle = "Dislike";
-          } else {
-            $scope.LikeTitle = "like";
+            const Purchase = Resource.new('purchase');
+            const Batch = Resource.new('item-batch');
+            Batch.query(/*{item: $scope.item._id}*/).$promise
+              .then(function (batch) {
+                //$scpoe.batch = batch[0];
+                var purchase = {
+                  item: $scope.item._id,
+                  batch: /*$scope.batch._id*/'572e5ca7a8070b345acf85ac',
+                  buyer: $scope.item.seller._id,
+                  time: Date.now(),
+                  numOfItems: Number(res),
+                  price: res * $scope.item.pricePerItem
+
+                };
+                Purchase.save(purchase).$promise.then(function (res) {
+                  $scope.purchase = res;
+
+                  $state.go('app.orderItem', {purchaseId: $scope.purchase._id});
+                });
+              });
+
           }
+        });
+
+      };
+
+      const Item = Resource.new("item", {
+        'ofSeller': {
+          method: 'GET',
+          params: {populate: 'seller img'}
         }
-        , function (err) {
-          console.error('Response error', err);
-        });
-    };
-
-
-    $scope.likeItem = function () {
-
-      var UserLikedItem = $scope.item.likes.filter(function (currUser) {
-        return currUser === userId;
       });
+      const userId = Authorization.getUser()._id;
 
-      // checks if not liked this item before
-      if (UserLikedItem.length) {
-        $scope.item.likes = $scope.item.likes.filter(function (currUser) {
-          return currUser !== userId;
+      var getItemById = function () {
+        Item.ofSeller({id: $stateParams.itemId})
+          .$promise.then(function (item) {
+
+            $scope.item = item;
+            if ($scope.item.likes.filter(function (currUser) {
+                return currUser === userId;
+              }).length) {
+              $scope.LikeTitle = "Dislike";
+            } else {
+              $scope.LikeTitle = "like";
+            }
+          }
+          , function (err) {
+            console.error('Response error', err);
+          });
+      };
+
+
+      $scope.likeItem = function () {
+
+        var UserLikedItem = $scope.item.likes.filter(function (currUser) {
+          return currUser === userId;
         });
-        $scope.LikeTitle = "Like";
-      } else {
-        $scope.item.likes.push(userId);
-        $scope.LikeTitle = "Dislike";
-      }
-      Item.update($scope.item).$promise
-        .catch(function (err) {
-          console.error(JSON.stringify(err));
-        });
-    };
+
+        // checks if not liked this item before
+        if (UserLikedItem.length) {
+          $scope.item.likes = $scope.item.likes.filter(function (currUser) {
+            return currUser !== userId;
+          });
+          $scope.LikeTitle = "Like";
+        } else {
+          $scope.item.likes.push(userId);
+          $scope.LikeTitle = "Dislike";
+        }
+        Item.update($scope.item).$promise
+          .catch(function (err) {
+            console.error(JSON.stringify(err));
+          });
+      };
 
 
-    getItemById();
+      getItemById();
 
-  }]);
+    }]);
--- www/app/item/order-item/order-item.controller.js
+++ www/app/item/order-item/order-item.controller.js
@@ -1,7 +1,7 @@
 angular.module('homemade')
-  .config(function($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
+  .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
     $stateProvider.state('app.orderItem', {
-      url: '/item/orderItem?itemId',
+      url: '/item/orderItem?purchaseId',
       views: {
         'menuContent': {
           templateUrl: 'app/item/order-item/order-item.html',
@@ -12,4 +12,14 @@ angular.module('homemade')
   })
   .controller('OrderItemCtrl', function ($scope, $stateParams, Resource, image, $timeout, ionicMaterialInk, ionicMaterialMotion, initIonicView) {
     initIonicView($scope, ionicMaterialInk, ionicMaterialMotion);
+    const Purchase = Resource.new('purchase', {
+      'withPopulate': {
+        method: 'GET',
+        params: {populate: 'item buyer'}
+      }
+    });
+    Purchase.withPopulate({id: $stateParams.purchaseId}).$promise
+      .then(function (purchase) {
+        $scope.purchase = purchase;
+      });
   });
