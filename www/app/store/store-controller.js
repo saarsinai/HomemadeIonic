ad0bc0015b274ff270442d9354e8de7d786b5d1b
saarsinai <saars7@gmail.com>
2016-05-07 15:03:38 +0300

    feat(item-view): add link from item display to the store of the seller

--- www/app/store/store-controller.js
+++ www/app/store/store-controller.js
@@ -1,7 +1,7 @@
 var app = angular.module("homemade")
   .config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider) {
     $stateProvider.state('app.store', {
-      url: '/store',
+      url: '/store/:sellerId',
       views: {
         'menuContent': {
           templateUrl: 'app/store/store.html',
@@ -13,7 +13,7 @@ var app = angular.module("homemade")
       }
     });
   })
-  .controller("StoreController", function ($scope, Resource, ionicMaterialInk, ionicMaterialMotion, initIonicView) {
+  .controller("StoreController", function ($scope, $stateParams , Resource, ionicMaterialInk, ionicMaterialMotion, initIonicView) {
     initIonicView($scope, ionicMaterialInk, ionicMaterialMotion);
 
     const User = Resource.new("user", {'items': {method: 'GET', relativeUrl: 'items', detail: true, isArray: true}});
@@ -29,28 +29,20 @@ var app = angular.module("homemade")
       console.error(JSON.stringify(err));
     };
 
-    // temporary - will be replaced when we will have the id of the store
-    User.query().$promise
-      .then(function (sellers) {
-        var aSeller = null;
-        sellers.forEach(function (s) {
-          if (s.store.active) {
-            aSeller = s;
-          }
-        });
-        $scope.seller = aSeller;
+    User.get({id: $stateParams.sellerId}).$promise
+    .then(function(seller){
+        $scope.seller = seller;
+      }).catch(logError);
 
-        User.items({id: $scope.seller._id}).$promise
-          .then(function (items) {
-            $scope.items = items;
-          })
-          .catch(logError);
+    User.items({id: $stateParams.sellerId}).$promise
+      .then(function (items) {
+        $scope.items = items;
+      })
+      .catch(logError);
 
-        Review.ofSeller({reviewed: $scope.seller._id}).$promise
-          .then(function (reviews) {
-            $scope.reviews = reviews;
-          })
-          .catch(logError);
+    Review.ofSeller({reviewed: $stateParams.sellerId}).$promise
+      .then(function (reviews) {
+        $scope.reviews = reviews;
       })
       .catch(logError);
   });
