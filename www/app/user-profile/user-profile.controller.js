7a8ceb3cbe9640a97a99fbe26fdb617e4a090975
rafi revach <rafi revach>
2016-05-13 16:47:48 +0300

    feat(user-edit-view): change the user profile view to be editable

--- www/app/user-profile/user-profile.controller.js
+++ www/app/user-profile/user-profile.controller.js
@@ -10,9 +10,10 @@ angular.module('homemade')
       }
     })
   })
-  .controller('UserProfileCtrl', function ($scope, $stateParams, Resource, image, $timeout, ionicMaterialInk, ionicMaterialMotion, Authorization, initIonicView) {
+  .controller('UserProfileCtrl', function ($scope, $stateParams, Resource, image, $timeout, $ionicPopup, ionicMaterialInk, ionicMaterialMotion, Authorization, initIonicView, saveOverlay) {
     initIonicView($scope, ionicMaterialInk, ionicMaterialMotion);
 
+    const User = Resource.new("user");
     const Purchase = Resource.new("purchase", {
       'ofUser': {
         method: 'GET',
@@ -22,12 +23,13 @@ angular.module('homemade')
     });
 
     $scope.profile = Authorization.getUser();
+
     Purchase.ofUser({buyer: $scope.profile._id}).$promise
       .then(function (purchases) {
         const Img = Resource.new("img");
 
         // TODO 1: fix this horrible query
-        // make beter query on the server side that populate purchase.item.img
+        // make better query on the server side that populate purchase.item.img
         // using one query and not many query per img
         angular.forEach(purchases, function (purchase, key) {
           Img.get({id: purchase.item.img}).$promise.then(function (image, err) {
@@ -39,5 +41,68 @@ angular.module('homemade')
         })
         $scope.purchases = purchases;
       });
+
+    var validateUser = function (profile) {
+
+      if (!profile.name || profile.name.length === 0) {
+        $ionicPopup.alert({
+          title: 'Cannot save user',
+          template: 'Please enter your name'
+        });
+        return false;
+      }
+
+      if (!profile.email || profile.email.length === 0) {
+        $ionicPopup.alert({
+          title: 'Cannot save user',
+          template: 'Please enter your email address'
+        });
+        return false;
+      }
+
+      var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (!emailRegex.test(profile.email)) {
+        $ionicPopup.alert({
+          title: 'Cannot save user',
+          template: 'Please enter a valid email address'
+        });
+        return false;
+      }
+
+      if (!profile.age || profile.age.length === 0) {
+        $ionicPopup.alert({
+          title: 'Cannot save user',
+          template: 'Please enter your age'
+        });
+        return false;
+      }
+
+      var ageRegex = /^\d+$/;
+
+      if (!ageRegex.test(profile.age)) {
+        $ionicPopup.alert({
+          title: 'Cannot save user',
+          template: 'Please enter age with numbers only'
+        });
+        return false;
+      }
+      return true;
+    };
+
+    $scope.saveUser = function () {
+      if (validateUser($scope.profile)) {
+        saveOverlay.show($scope);
+
+        User.update($scope.profile).$promise
+          .then(function (res) {
+            return saveOverlay.success();
+          },
+          function (err) {
+            console.error(JSON.stringify(err));
+            return saveOverlay.error();
+          });
+      }
+    };
   })
 ;
