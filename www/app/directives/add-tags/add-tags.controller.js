cfb1591f64371b8e3bbc55d52319d43495734703
rafi revach <rafi revach>
2016-06-10 17:22:07 +0300

    feat(add-tags): created add tags directive

---
+++ www/app/directives/add-tags/add-tags.controller.js
@@ -0,0 +1,21 @@
+angular.module('homemade')
+  .directive('hmdAddTags', function() {
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'app/directives/add-tags/add-tags.html',
+      scope: {
+        item: '='
+      },
+      controller: function ($scope) {
+
+        $scope.item.tags = [];
+        $scope.newTag = "";
+
+        $scope.addTag = function() {
+          $scope.item.tags.push($scope.newTag);
+          $scope.newTag = "";
+        };
+      }
+    };
+  });
